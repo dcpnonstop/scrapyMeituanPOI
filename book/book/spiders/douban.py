c9c87cefbff0ab62da724f00c1d922158407d18f
Hordor <632013350@qq.com>
2018-03-28 14:32:07 +0800

    Update douban.py
    用yield增加自动抓取下一页,将抓取的内容以追加的方式写入txt
    也就是将新获取的request返回给引擎，实现继续循环。也就实现了“自动下一网页的爬取”

--- book/book/spiders/douban.py
+++ book/book/spiders/douban.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.selector import Selector
+from scrapy.http import Request
 
 class DoubanSpider(scrapy.Spider):
     name = 'douban'
@@ -19,6 +20,13 @@ class DoubanSpider(scrapy.Spider):
             # rate = each.xpath('div[@class="rating"]/span[@class="rating_nums"]/text()').extract()[0]
             # author = re.search('<div>(.*?)<br', each.extract(), re.S).group(1)
             result +="\n"+ title
+        #自动抓取下一页,将新获取的request返回给引擎，实现继续循环。也就实现了“自动下一网页的爬取”。
+        for i in range(2,21):
+            url = 'http://i.meituan.com/s/a?cid=-1&bid=-1&sid=defaults&p='+str(i)+'&ciid=50&bizType=area&csp=&nocount=true&stid_b=_b2&w=%E7%A6%8F%E9%9B%B7%E5%BE%B7'
+            # print(url)
+            yield Request(url, callback=self.parse)
+            
+        #抓取的内容以追加的方式写入txt
         f =open('POIname2.txt',"w")
         f.write(result)
         f.close()
